03f83a1c2be4d6d5bc905330601060f6abb7b934
GouravSingh2005 <gouravsingh56466@gmail.com>
2025-12-14 22:54:07 +0530

    Fixed all things

--- Frontend/my-project/src/App.jsx
+++ Frontend/my-project/src/App.jsx
@@ -24,7 +24,7 @@ export default function App() {
     formData.append("file", file);
 
     try {
-      const res = await axios.post("http://localhost:5000/upload", formData, {
+      const res = await axios.post("https://quickshare-e2lz.onrender.com/upload", formData, {
         headers: { "Content-Type": "multipart/form-data" },
         onUploadProgress: (e) => {
           if (!e.total) return;
--- Frontend/my-project/src/main.jsx
+++ Frontend/my-project/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import ReactGA from 'react-ga4';
+
+// Initialize GA4 with your Measurement ID
+ReactGA.initialize('G-9H6QCHZDBN');
+
+function Root() {
+  // Track first pageview
+  useEffect(() => {
+    ReactGA.send('pageview');
+  }, []);
+
+  return <App />;
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
-)
+);
